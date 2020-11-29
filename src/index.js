e49061eb0d1b74e702f9482c1c5fc85377fa6a3e
thailephan <thailephanminh@gmail.com>
2020-11-29 15:39:14 +0700

    End React and Prepare for using with Redux toolkit - a revolution

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
+import 'antd/dist/antd.css';
+import './sass/style.sass'
+
+import store from './redux'
+import {Provider} from 'react-redux'
+
+import {BrowserRouter as Router} from 'react-router-dom'
+import RouteConfig from './components/RouteConfig';
+
+import {actions} from './redux/reducers/index'
+
+
+import 'moment/locale/vi'
+import locale from 'antd/es/date-picker/locale/vi_VN'
+import { ConfigProvider } from 'antd';
+
+function App() {
+  return (
+    <ConfigProvider locale = {locale}>
+      <Provider store = {store}>
+          <Router forceRefresh = {true}>
+                <RouteConfig />
+          </Router>
+        </Provider>
+    </ConfigProvider>
+  );
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
+
+// store.dispatch(actions.increment(2))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
